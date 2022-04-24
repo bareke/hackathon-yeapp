f4dc50820f1166e002412d4fcb8d22fcceabd7b9
Cristian Ordoñez <cristiano_ord93@hotmail.com>
2022-04-24 09:16:19 -0500

    Add analize medical history

--- frontend/src/components/ClinicHistory/ViewClinicHistory.vue
+++ frontend/src/components/ClinicHistory/ViewClinicHistory.vue
@@ -3,6 +3,59 @@
     <vs-button danger flat class="absolute right-4 my-4" @click="generateReport"
       >Generar reporte</vs-button
     >
+    <div class="center">
+      <vs-button
+        success
+        flat
+        class="absolute right-40 my-4"
+        @click="active = !active"
+        >Analizar historia</vs-button
+      >
+      <vs-dialog scroll overflow-hidden not-close auto-width v-model="active">
+        <template #header>
+          <h3>
+            <b
+              >Recomendaciones teniendo en cuenta tu reciente historial
+              clínico</b
+            >
+          </h3>
+        </template>
+        <div class="con-content">
+          <h4>
+            <b>{{ this.recommendations.title }}</b>
+          </h4>
+          <p>
+            {{ this.recommendations.description }}
+          </p>
+          <br />
+          <br />
+          <h4>
+            <b>{{ this.recommendations.limits.title }}</b>
+          </h4>
+
+          <br />
+
+          <p>
+            {{ this.recommendations.limits.recommendations[0].title }}
+            <br />
+            <br />
+            {{ this.recommendations.limits.recommendations[0].description }}
+
+            <br />
+            <br />
+
+            {{ this.recommendations.limits.recommendations[1].title }}
+            <br />
+            <br />
+            {{ this.recommendations.limits.recommendations[1].description }}
+          </p>
+
+          <vs-button text-center success flat @click="scheduleAppointment"
+            >Agendar cita</vs-button
+          >
+        </div>
+      </vs-dialog>
+    </div>
     <AccoredeonComponent
       class="mt-16"
       ref="user"
@@ -48,19 +101,57 @@
     >
   </div>
 </template>
+
 <script>
 import AccoredeonComponent from "@/components/AccordeonComponent.vue";
+
 export default {
   components: { AccoredeonComponent },
   props: ["clinicHistory"],
   data() {
-    return {};
+    return {
+      active: false,
+      recommendations: {},
+    };
+  },
+  created() {
+    const option = "colesterol";
+    const description = this.clinicHistory.node.data.consultData.description;
+    if (description.includes(option)) {
+      this.recommendations = {
+        title: "¿Qué es el colesterol?",
+        description:
+          "Su cuerpo necesita algo de colesterol para funcionar bien. Pero si tiene demasiado colesterol en la sangre, se puede pegar en las paredes de sus arterias, estrechándolas o incluso bloqueándolas. Esto lo pone en riesgo de enfermedad de las arterias coronarias y otras enfermedades del corazón.",
+        limits: {
+          title: "¿Cómo puedo bajar el colesterol con la dieta?",
+          recommendations: [
+            {
+              title: "Limite la sal.",
+              description: `Debe intentar limitar la cantidad de sodio (sal) que consume a no más de 2,300 miligramos (aproximadamente una cucharadita de sal) por día. Eso incluye toda la sal que consume, ya sea que se haya agregado en la cocina o en la mesa, o que ya esté presente en los productos alimenticios. Limitar la sal no reducirá el colesterol, pero puede bajar el riesgo de enfermedades cardíacas al ayudar a reducir la presión arterial. Puede reducir la sal eligiendo alimentos con bajo contenido de sal y "sin sal agregada", además de preferir condimentos en la mesa o al cocinar en vez de sal.`,
+            },
+            {
+              title: "Limite el alcohol.",
+              description:
+                "El alcohol añade calorías adicionales, las que pueden llevar al aumento de peso. Tener sobrepeso puede elevar su nivel de colesterol malo y disminuir su nivel de colesterol bueno. Demasiado alcohol también puede aumentar su riesgo de enfermedades del corazón, porque puede elevar su presión arterial y el nivel de triglicéridos.",
+            },
+          ],
+        },
+      };
+    }
   },
   methods: {
     generateReport() {
       this.launchNotification(
         "success",
         "Generando reporte ...",
+        "",
+        '<box-icon color="rgb(20,255,20)" type="solid" name="newUser-check"></box-icon>'
+      );
+    },
+    scheduleAppointment() {
+      this.launchNotification(
+        "success",
+        "Programando cita preventiva ...",
         "",
         '<box-icon color="rgb(20,255,20)" type="solid" name="newUser-check"></box-icon>'
       );
